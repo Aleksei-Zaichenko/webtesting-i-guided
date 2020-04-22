0bbf00fe9ba1c09974c9f4dc65c6065da00034f4
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-04-22 13:49:38 -0400

    added testing

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,6 +2,15 @@ module.exports = {
   add,
 };
 
-function add() {
-  return null;
+function add(args) {
+// return numbers.reduce((sum, number) =>{
+//   return sum + number;
+// },0)
+  const numbers = Array.isArray(args) ? args : Array.from(arguments);
+
+  let sum = 0;
+  for(let i = 0; i < numbers.length; i++){
+    sum += numbers[i];
+  }
+  return Number(sum);
 }
--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,37 @@
 const { add } = require('./calculator.js');
 
 // test away!
+describe('calculator.js', ()=>{
+    it("should run tests without errors",()=>{
+        expect(true).toBeTruthy();
+    })
+
+    describe('.add', () => {
+        it('should add two numbers', ()=>{
+            const sum = add(3,3);
+
+            expect(sum).toBe(6);
+        });
+
+        it('should return 0 when called with no arguments',()=>{
+            const shouldBeZero = add();
+            expect(shouldBeZero).toBe(0);
+        });
+
+        it('should return the number when called with a single number',()=>{
+            expect(add(3)).toBe(3);
+            expect(add(-5)).toBe(-5);
+            expect(add(-5 + 10)).toBe(5);
+        });
+
+        it('should sum a list of number separated by comma',()=>{
+            expect(add(1,2,3)).toBe(6);
+            expect(add(101,-32,31)).toBe(100);
+        });
+
+        it('should support an array of numbers',()=>{
+            expect(add([1,2,3])).toBe(6);
+            expect(add([101,-32,31])).toBe(100);
+        });
+    })
+})
